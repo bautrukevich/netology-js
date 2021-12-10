d8775a3b4d45b4a23264fc3a844e8970e8a53d5c
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-12-11 01:19:41 +0400

    fix: entry point

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,12 +2,19 @@ import './index.css';
 import { render } from '../utils/render';
 import { App } from '../components/App/App';
 import { getData } from '../api/getData';
+import { ErrorMessage } from '../components/ErrorMessage/ErrorMessage';
 
 (async () => {
   const rootElement = document.getElementById('root');
-  const app = App({ isLoading: true, data: [] });
-  render(app, rootElement);
 
-  const data = await getData();
-  render(App({ isLoading: false, data }), app);
+  try {
+    const app = App({ isLoading: true, data: [] });
+    render(app, rootElement);
+
+    const data = await getData();
+    const appWithData = App({ isLoading: false, data });
+    render(appWithData, rootElement);
+  } catch (error) {
+    render(ErrorMessage({ message: error.message }), rootElement);
+  }
 })();
