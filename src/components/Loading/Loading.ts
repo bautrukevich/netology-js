114b681f6b7ce8e9bf013e4039cf780d5e92998c
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-12-11 01:18:11 +0400

    fix: Loading component

--- src/components/Loading/Loading.ts
+++ src/components/Loading/Loading.ts
@@ -3,6 +3,13 @@ import { html } from '../../utils/html';
 
 export function Loading(): Element {
   return html`
-    <div class="loading">Loading...</div>
+    <div class="loading">
+      <div class="loading__container">
+        <div class="loading__ripple">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
   `;
 }
