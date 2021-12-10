a1c7b780bf427dcb0ece7142936cadbde9254af7
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-12-10 16:41:42 +0400

    feat: init app in index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,13 @@
 import './index.css';
+import { render } from '../utils/render';
+import { App } from '../components/App/App';
+import { getData } from '../api/getData';
 
-// eslint-disable-next-line no-console
-console.log('Hello, world!');
+(async () => {
+  const rootElement = document.getElementById('root');
+  const app = App({ isLoading: true, data: [] });
+  render(app, rootElement);
+
+  const data = await getData();
+  render(App({ isLoading: false, data }), app);
+})();
