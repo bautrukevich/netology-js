8b2aab9ee4f0a7ae1e84c7f99807c1b118a5e715
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-12-11 01:18:59 +0400

    feat: add circle to List item and fix styles

--- src/components/List/List.ts
+++ src/components/List/List.ts
@@ -1,14 +1,17 @@
 import './List.css';
 import { html } from '../../utils/html';
-import { Item } from '../../api/getData';
+import type { Item } from '../../api/getData';
 
-function ListItem({ title, itemsCount }: Item): Element {
+function ListItem({
+  title, itemsCount, link,
+}: Item): Element {
   return html`
     <li class="list__item">
-      <a class="list__link" href="">
+      <a class="list__link" href="${link}">
         <span class="list__item-title">${title}</span>
         <span class="list__item-count">${itemsCount}</span>
       </a>
+      <div class="list__circle"></div>
     </li>
   `;
 }
@@ -16,6 +19,10 @@ function ListItem({ title, itemsCount }: Item): Element {
 export function List({ data }: { data: Item[] }): Element {
   return html`
     <ul class="list">
-      ${data.map(({ title, itemsCount }) => ListItem({ title, itemsCount }))}
+      ${data.map(({
+    title, itemsCount, link,
+  }) => ListItem({
+    title, itemsCount, link,
+  }))}
     </ul>`;
 }
